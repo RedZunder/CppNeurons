e5589f087bb332dfcd669b24107c4ba8d1448052
RedZunder <alt47r@gmail.com>
2025-06-11 13:18:54 +0200

    Moving to food and reproducing

--- classes.h
+++ classes.h
@@ -3,7 +3,12 @@
 using namespace sf;
 
 
-
+/**
+*   @brief          Return the length of a sf::Vector2f vector
+*   @param  vec     The vector
+*
+*   @return         Length of vector
+*/
 float getV2Length(sf::Vector2f vec)
 {
     return sqrt(vec.x * vec.x + vec.y * vec.y);
@@ -12,34 +17,55 @@ float getV2Length(sf::Vector2f vec)
 
 
 
+
+
+
 class Cell : public CircleShape {
 public:
-    float speed;
-
-
-    Cell(float rad, Color color, float speed_, Vector2f pos = Vector2f(0.0f, 0.0f))
+    float speedx, speedy, energy;
+    int top_edge, btm_edge, left_edge, rgt_edge;
+    int vector_index=0;
+    Cell(float rad, Color color, float speed_, float energy_, Vector2f pos = Vector2f(0.0f, 0.0f))
     {
         setPosition(pos);
         setRadius(rad);
+        setOrigin(getRadius(), getRadius());
         setFillColor(color);
-        speed = speed_;
+        speedx = speed_;
+        speedy = speed_;
+        energy = energy_;
+
+        top_edge = roundf(getGlobalBounds().top);
+        btm_edge = roundf(getGlobalBounds().height);
+        left_edge = roundf(getGlobalBounds().left);
+        rgt_edge = roundf(getGlobalBounds().width);
     }
 
 
     void moveTo(Vector2f direction, float time_ms)
     {
-        move(Vector2f((direction.x * time_ms * speed) / getV2Length(direction), (direction.y * time_ms * speed) / getV2Length(direction)));
+        move(Vector2f((direction.x * time_ms * speedx) / getV2Length(direction), (direction.y * time_ms * speedy) / getV2Length(direction)));
+
+#if DEBUG==1
 
         std::cout << "Direction " << direction.x / getV2Length(direction) << " " << direction.y / getV2Length(direction)
             << "  Distance " << getV2Length(direction) << std::endl;
+        std::cout << "Borders " << top_edge << " " << btm_edge << " " << left_edge << " " << rgt_edge <<std::endl;
+#endif
     }
 
 
 
+
+
+
+
+#if DEBUG==1
     void showInfo()                                                                                     //For debugging
     {
         std::cout << getPosition().x << " " << getPosition().y << "  " << getRadius()<< std::endl;
     }
+#endif
 
 
 
@@ -53,14 +79,36 @@ class Food : public CircleShape
 {
 public:
     float energy;
+    int top_edge, btm_edge, left_edge, rgt_edge;
 
-    Food(float en) { energy = en; }
+    Food(float en, float radius) 
+    {
+        energy = en;
+        top_edge = roundf(getGlobalBounds().top);
+        btm_edge = roundf(getGlobalBounds().height);
+        left_edge = roundf(getGlobalBounds().left);
+        rgt_edge = roundf(getGlobalBounds().width);
+        setRadius(radius);
+        setOrigin(getRadius(), getRadius());
+    }
 
 
 
 };
 
 
+void reproduceCell(Cell* cell, std::vector<Cell*> *drawQueue, Vector2f offset)
+{
+    static Cell c1(cell->getRadius(), cell->getFillColor(), cell->speedx, cell->energy, cell->getPosition() + offset);
+    
+    static Cell c2(cell->getRadius(), cell->getFillColor(), cell->speedx, cell->energy,cell->getPosition() -offset);
+
+    drawQueue->emplace_back(&c1);
+    drawQueue->emplace_back(&c2);
+
+    drawQueue->erase(drawQueue->begin() + cell->vector_index);
+    std::cout << "Size of vector " << drawQueue->size();
+}
 
 
 
--- main.cpp
+++ main.cpp
@@ -5,38 +5,48 @@ using namespace std;
 
 int main()
 {
+	//-----------------------------------------DECLARATIONS
+
+	std::vector<Cell*> drawQueue;
+
 	sf::Clock clock;
 	sf::RenderWindow window(sf::VideoMode(720, 720), "Screen");
 
 	sf::Vector2f screenCenter(window.getSize().x/2, window.getSize().y / 2);
 
-
+	
 	sf::Color col;
 	col.r = 255; col.g = 255; col.b = 255;
 
-	Cell luca(50, col, 100);
+	Cell luca(30, col, 80, 150);
+	drawQueue.emplace_back(&luca);
+	luca.vector_index = drawQueue.size() - 1;
 
 	luca.setPosition(screenCenter.x - luca.getRadius(), screenCenter.y - luca.getRadius());
 
+
+#if DEBUG==1
 	luca.showInfo();
+#endif
 
 
-
-	Food snack(30);
-	snack.setRadius(10);
+	Food snack(30, 10);
 	snack.setFillColor(sf::Color::Blue);
 	snack.setPosition(Vector2f(100,50));
 
 	Vector2f foodDir(0, 0);
 
+	//-----------------------------------------
 
 
+
+	//-----------------------------------------MAIN LOOP
 	while (window.isOpen())
 	{
 		window.setFramerateLimit(60);
 		sf::Time elapsed = clock.restart();
 
-	srand(time(NULL));
+		srand(time(NULL));
 
 
 
@@ -51,18 +61,25 @@ int main()
 
 
 
-		//----------------------CALCULATE MOVEMENT
+		//-----------------------------------------CALCULATE MOVEMENT
 
 		foodDir = (snack.getPosition() - luca.getPosition());
 
 
-		//approach until too close
-		if (getV2Length(foodDir) > 1)
+		//approach until touch
+		if (getV2Length(foodDir) > snack.getRadius() + luca.getRadius())
 		{
 			luca.moveTo(foodDir, elapsed.asSeconds());
 		}
+		else if (drawQueue.at(luca.vector_index) == &luca)
+		{
+			reproduceCell(&luca, &drawQueue, sf::Vector2f(rand() % 30 + 30, rand() % 20 + 10));
+		}
+		
+		//-----------------------------------------
 
-		//----------------------END OF CALCULATE MOVEMENT
+
+
 
 
 		// clear the window with black color
@@ -71,7 +88,11 @@ int main()
 
 
 		///DRAW THINGS
-		window.draw(luca);
+		for (auto i : drawQueue)
+		{
+			window.draw(*i);
+		}
+
 		window.draw(snack);
 
 
